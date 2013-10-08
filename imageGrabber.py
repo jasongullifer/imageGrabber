7c4f5567be0611f883067aa5f1ac70ab9e384192
Jason W. Gullifer <jason.gullifer@gmail.com>
2013-10-08 12:39:09 -0400

    spiffing

--- imageGrabber.py
+++ imageGrabber.py
@@ -3,11 +3,11 @@ import os
 import Image
 import xlrd
 import csv
+
 csvFile = csv.DictReader(open('testFillers.csv'))
 pictureDir = './pictures/'
 convertedDir = './pictures/converted/'
-errorFile = open('errors.txt','w')
-
+errorFile = open('errors.txt', 'w')
 
 lines = list(csvFile)
 numLines = len(lines)
@@ -18,24 +18,18 @@ if not os.path.exists(pictureDir):
 if not os.path.exists(convertedDir):
     os.makedirs(convertedDir)
 
-
-# line = dict()
-# line['Item Number'] = 1
-# line['TargetWord'] = 'rabbithat'
-# line['ImageLink'] =  'http://blogs.houstonpress.com/hairballs/rabbit-hat-small040110.jpg'
-
 lineNum = 1
 for line in lines: 
     print 'Working on line ' + str(lineNum) + ' of ' + str(numLines) + '.'
     fileName, fileType = os.path.splitext(line['ImageLink'])
-    fileType=fileType.split('?')[0]
+    fileType = fileType.split('?')[0]
     try:
-        urllib.urlretrieve(line['ImageLink'],pictureDir+str(line['Item Number'])+'_'+line['Target Word']+fileType)
-        im = Image.open(pictureDir+str(line['Item Number'])+'_'+line['Target Word']+fileType)
-        im.save(convertedDir+str(line['Item Number'])+'_'+line['Target Word']+'.png')
+        urllib.urlretrieve(line['ImageLink'], pictureDir + str(line['Item Number']) + '_' + line['Target Word'] + fileType)
+        im = Image.open(pictureDir + str(line['Item Number']) + '_' + line['Target Word'] + fileType)
+        im.save(convertedDir + str(line['Item Number']) + '_' + line['Target Word'] + '.png')
     except IOError:
-        print 'Can\'t open image'
-        errorFile.write(str(line)+'\n')
+        print 'Can\'t open image for item number ' + str(line['Item Number'])
+        errorFile.write(str(line) + '\n')
     lineNum += 1
         
 errorFile.close()
