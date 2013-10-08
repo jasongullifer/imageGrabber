42673d33d7a58a74f2c8be4d75347ba2339bf87a
Jason W. Gullifer <jason.gullifer@gmail.com>
2013-10-08 13:21:53 -0400

    skip existing, don't write converted image if original image fails

--- imageGrabber.py
+++ imageGrabber.py
@@ -7,29 +7,45 @@ import csv
 csvFile = csv.DictReader(open('testFillers.csv'))
 pictureDir = './pictures/'
 convertedDir = './pictures/converted/'
+convertedFileType = 'png'
 errorFile = open('errors.txt', 'w')
 
 lines = list(csvFile)
 numLines = len(lines)
 
 if not os.path.exists(pictureDir):
+    print 'Creating picture directory'
     os.makedirs(pictureDir)
     
 if not os.path.exists(convertedDir):
+    print 'Creating converted picture directory'
     os.makedirs(convertedDir)
 
 lineNum = 1
-for line in lines: 
-    print 'Working on line ' + str(lineNum) + ' of ' + str(numLines) + '.'
+
+for line in lines:
+    flag=True
+    print '\nWorking on line ' + str(lineNum) + ' of ' + str(numLines) + '.'
     fileName, fileType = os.path.splitext(line['ImageLink'])
     fileType = fileType.split('?')[0]
-    try:
-        urllib.urlretrieve(line['ImageLink'], pictureDir + str(line['Item Number']) + '_' + line['Target Word'] + fileType)
-        im = Image.open(pictureDir + str(line['Item Number']) + '_' + line['Target Word'] + fileType)
-        im.save(convertedDir + str(line['Item Number']) + '_' + line['Target Word'] + '.png')
-    except IOError:
-        print 'Can\'t open image for item number ' + str(line['Item Number'])
-        errorFile.write(str(line) + '\n')
+
+    originalFile = pictureDir + str(line['Item Number']) + '_' + line['Target Word'] + fileType
+    convertedFile = convertedDir + str(line['Item Number']) + '_' + line['Target Word'] + '.' + convertedFileType
+
+    if not os.path.isfile(convertedFile):
+        try:
+            urllib.urlretrieve(line['ImageLink'], originalFile)
+            im = Image.open(originalFile)
+        except IOError:
+            flag=False
+            print '    Can\'t retrieve image for item number ' + str(line['Item Number'])
+            errorFile.write(str(line) + '\n')
+
+        if flag:
+            im.save(convertedFile)
+            
+    else:
+        print '    File "' + os.path.split(convertedFile)[1]  + '" already exists. Skipping...'
     lineNum += 1
         
 errorFile.close()
